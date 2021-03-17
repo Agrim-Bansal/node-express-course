32abfc84bfe2ae255090fffd0cf372a46c1552e4
Agrim Bansal <agrimx2@gmail.com>
2021-03-17 15:26:06 +0530

    Add 1st get route

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 
+mock_user_data = [
+  {'name' : 'Agrim'},
+  {'name' : 'Arshita'}
+]
+
+app.get('/users', (req, res) => {
+    res.json({
+      success : true,
+      message : 'Success!',
+      users : mock_user_data
+    })
+})
+
+
 app.listen(8000, () => {
   console.log('Hello World');
 });
